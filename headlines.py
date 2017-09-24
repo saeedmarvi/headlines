f40f01f2d3ff9c1e1367084377534064110b47e9
stonefl <lfengstone@gmail.com>
2017-09-24 13:42:43 -0500

    add source code

---
+++ headlines.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def get_news():
+    return "no news is good news"
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
