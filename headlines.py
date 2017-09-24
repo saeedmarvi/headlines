086780648b789589f7fee6f6de05038786d06bd7
stonefl <lfengstone@gmail.com>
2017-09-24 14:08:51 -0500

    update source multiple news sources

--- headlines.py
+++ headlines.py
@@ -2,16 +2,35 @@
 """
 Spyder Editor
 
-This is a temporary script file.
+This is a python script file.
 """
-
+import feedparser
 from flask import Flask
 
 app = Flask(__name__)
 
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+             'cnn': 'http://rss.cnn.com/rss/edition.rss',
+             'fox': 'http://feeds.foxnews.com/foxnews/latest',
+             'iol': 'http://www.iol.co.za/cmlink/1.640'}
+
 @app.route("/")
-def get_news():
-    return "no news is good news"
+#In Flask, if we specify a part of our URL path in angle brackets < >, 
+# then it is taken as a variable and is passed to our application code. 
+@app.route("/<publication>")
+
+def get_news(publication="bbc"):
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    first_article = feed['entries'][0]
+    return """<html>
+      <body>
+        <h1> BBC Headlines </h1>
+        <b>{0}</b> <br/>
+        <i>{1}</i> <br/>
+        <p>{2}</p> <br/>
+      </body>
+     </html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
